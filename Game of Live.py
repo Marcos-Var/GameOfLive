fa2748793c3f53ce8a28e56b2355d019b8e6faf8
Marcos Vargas Azero <mvadvrock@gmail.com>
2024-08-20 12:51:04 -0400

    Se agregaron comentarios

--- Game of Live.py
+++ Game of Live.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pygame
-
 #Librerias creadas
-
 pygame.init()
 
 width, height = 1000, 1000
 screen = pygame.display.set_mode((height,width))
 
+#color de fondo casi negro oscuro
 bg = 25, 25, 25
+#pintando el fondo
+screen.fill(bg)
 
+# Bucle en ejecución
+while True:
+    pass
