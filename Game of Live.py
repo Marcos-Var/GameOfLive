ca7444d0f485de64159899032432c729ff171fae
Marcos Vargas Azero <mvadvrock@gmail.com>
2024-09-04 23:16:04 -0400

    Se corrigen mas errores

--- Game of Live.py
+++ Game of Live.py
@@ -5,16 +5,16 @@ import time
 #Librerias creadas
 pygame.init()
 
-width, height = 800, 800
+width, height = 700, 700
 screen = pygame.display.set_mode((height,width))
 
 #color de fondo casi negro oscuro
 bg = 25, 25, 25
 #pintando el fondo
+screen.fill(bg)
 
 
-
-nxC, nyC = 30, 30
+nxC, nyC = 50, 50
 
 dimCW = width/nxC
 dimCH = height/nyC
@@ -26,11 +26,14 @@ gameState = np.zeros((nxC, nyC))
 
 #creamos automatas para probar
 
-#automata palo
-gameState[5,3] =1
-gameState[5,4] =1
-gameState[5,5] =1
+#AUTOMATAS   (puedes activarlos)
 
+#nave basica
+gameState[21,21] = 1
+gameState[22,22] = 1
+gameState[22,23] = 1
+gameState[21,23] = 1
+gameState[20,23] = 1
 
 
 
@@ -40,7 +43,7 @@ while True:
     newGameState = np.copy(gameState)
 
     screen.fill(bg)
-    time.sleep(0.1)
+    time.sleep(0.07)
 
     for y in range(0, nxC):
         for x in range(0, nyC):
@@ -57,19 +60,19 @@ while True:
                       gameState[(x+1) % nxC, (y+1) % nyC]
             
 
-            # Regla 1 : Un celula muesta con mas de 3 vecinas vivas, revive
-            if gameState[x, y] == 0 and n_neigt == 3:
-                newGameState[x, y] = 1
+            # Regla 1 : Un celula muerta con mas de 3 vecinas vivas, revive
+            if gameState[x, y] == 1 and (n_neigt < 2 or n_neigt > 3):
+                newGameState[x, y] = 0
 
             # Regla 2 : un celula viva con menos de 2 o mas de 3 vecinas vivas, muere
-            elif gameState[x, y] == 1 and n_neigt <= 2 or n_neigt > 3:
-                newGameState[x, y] = 0
+            elif gameState[x, y] == 0 and n_neigt == 3:
+                newGameState[x, y] = 1
 
             #Creamos el pologono de cada celda a dibujar.
             poly = [((x)   * dimCW, y    * dimCH),
                     ((x+1) * dimCW, y    * dimCH),
                     ((x+1) * dimCW, (y+1) *dimCH ),
-                    ((x)   * dimCW, (y+1)    * dimCH)]
+                    ((x)   * dimCW, (y+1) * dimCH)]
             
 
             # Y dibujamos la celda para cada par de x e y
